f456f7e4c4d6237d9934a7e17ade2e537b2c468f
Ali Hassan <alihassankp27@gmail.com>
2022-04-13 23:42:31 +0400

    Add swiperjs package

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '@/styles/custom'
+import '@/styles/custom.css'
 
 createApp(App).use(store).use(router).mount('#app')
