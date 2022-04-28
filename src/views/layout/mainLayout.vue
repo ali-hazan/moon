e8aa40208033db10ef13772fa5d2bac421e70d99
Ali Hassan <alihassankp27@gmail.com>
2022-04-28 23:34:35 +0400

    Update navigation style

--- src/views/layout/mainLayout.vue
+++ src/views/layout/mainLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <a-layout has-sider>
     <a-layout-sider
-    v-model:collapsed="collapsed"
+      v-model:collapsed="collapsed"
       :style="{
         overflow: 'auto',
         height: '90vh',
@@ -10,48 +10,54 @@
         top: '16px',
         bottom: '16px',
         borderRadius: '48px',
-        overflowX: 'hidden'
+        overflowX: 'hidden',
       }"
     >
-      <div class="logo" />
-      <a-menu v-model:selectedKeys="selectedKeys"  mode="inline">
-        <a-menu-item key="1">
-          <user-outlined />
-          <span class="nav-text">nav 1</span>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <video-camera-outlined />
-          <span class="nav-text">nav 2</span>
-        </a-menu-item>
-        <a-menu-item key="3">
-          <upload-outlined />
-          <span class="nav-text">nav 3</span>
-        </a-menu-item>
-        <a-menu-item key="4">
-          <bar-chart-outlined />
-          <span class="nav-text">nav 4</span>
-        </a-menu-item>
-        <a-menu-item key="5">
-          <cloud-outlined />
-          <span class="nav-text">nav 5</span>
-        </a-menu-item>
-        <a-menu-item key="6">
-          <appstore-outlined />
-          <span class="nav-text">nav 6</span>
-        </a-menu-item>
-        <a-menu-item key="7">
-          <team-outlined />
-          <span class="nav-text">nav 7</span>
-        </a-menu-item>
-        <a-menu-item key="8">
-          <shop-outlined />
-          <span class="nav-text">nav 8</span>
-        </a-menu-item>
-      </a-menu>
+      <div>
+        <div class="logo" />
+        <a-menu v-model:selectedKeys="selectedKeys" mode="inline">
+          <a-menu-item key="1">
+            <user-outlined />
+            <span class="nav-text">nav 1</span>
+          </a-menu-item>
+          <a-menu-item key="2">
+            <video-camera-outlined />
+            <span class="nav-text">nav 2</span>
+          </a-menu-item>
+          <a-menu-item key="3">
+            <upload-outlined />
+            <span class="nav-text">nav 3</span>
+          </a-menu-item>
+          <a-menu-item key="4">
+            <bar-chart-outlined />
+            <span class="nav-text">nav 4</span>
+          </a-menu-item>
+          <a-menu-item key="5">
+            <cloud-outlined />
+            <span class="nav-text">nav 5</span>
+          </a-menu-item>
+          <a-menu-item key="6">
+            <appstore-outlined />
+            <span class="nav-text">nav 6</span>
+          </a-menu-item>
+          <a-menu-item key="7">
+            <team-outlined />
+            <span class="nav-text">nav 7</span>
+          </a-menu-item>
+          <a-menu-item key="8">
+            <shop-outlined />
+            <span class="nav-text">nav 8</span>
+          </a-menu-item>
+        </a-menu>
+      </div>
 
-      <a-button class="nav-collapse-btn" @click="collapsed=!collapsed">Togle</a-button>
+      <a-button class="nav-collapse-btn" @click="collapsed = !collapsed"
+        >Togle</a-button
+      >
     </a-layout-sider>
-    <a-layout :style="collapsed?{ marginLeft: '96px' }:{ marginLeft: '216px' }">
+    <a-layout
+      :style="collapsed ? { marginLeft: '96px' } : { marginLeft: '216px' }"
+    >
       <a-layout-content :style="{ margin: '24px 16px 0', overflow: 'initial' }">
         <router-view />
       </a-layout-content>
@@ -106,9 +112,9 @@ export default defineComponent({
 [data-theme="dark"] .site-layout .site-layout-background {
   background: #141414;
 }
-.nav-collapse-btn{
-  position: absolute !important;
-  bottom: 12px;
-  left: 0px;
+.ant-layout-sider-children {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
 }
 </style>
